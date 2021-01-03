39e1aea9a02ea52f13c89a3a8c70488a069ccf4c
Yunzhe Yu <yunzheyu0529@gmail.com>
2021-01-02 17:26:58 -0800

    new version(average_length)

--- read-analysis.py
+++ read-analysis.py
@@ -1,6 +1,6 @@
 data = []
 count = 0
-
+total = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
@@ -8,6 +8,10 @@ with open('reviews.txt', 'r') as f:
 		if count%10000 == 0:
 			print(len(data))
 
-print(data[0])
-print('----------------')
-print(data[1])
+print('This text total has', len(data), 'files!')
+
+for d in data:
+	total = total + len(d)
+
+print('Average: ',float(total/count))
+#print('Average: ',float(total/len(data)))
